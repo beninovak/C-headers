9cd9329c3a57676f20af45f4c5673efed5d371f2
beninovak <beninonovakino@proton.me>
2025-05-18 20:19:27 +0200

    idek, some progress with returning file i guess...only line_count tho, no line info...

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+// #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 //include "old_read.h"
@@ -7,14 +8,16 @@ int main() {
     clock_t start_time = clock();
 
     FILE* fptr = fopen("text_1kb.txt", "rb");
-    long num_lines = file_read_get_lines(fptr);
+    //long num_lines = file_read_get_lines(fptr);
 
-    printf("Number of lines: %ld", num_lines);
+    //printf("Number of lines: %ld\n", num_lines);
 
-    
+    _file* file = file_read(fptr);
+    printf("Line count: %d", file->line_count);
 
-
-
+    // for(int i = 0; i < file->line_count; i++) {
+    //     printf("%u\n", file->lines[i].size);
+    // }
 
     double total_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
     printf("\nTime elapsed: %f\n", total_time);
--- read_file.h
+++ read_file.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 //#define BUFFER_SIZE 16777216 -> TOO BIG
 #define BUFFER_SIZE 65536 // Kinda arbitrary ngl...
@@ -15,24 +17,33 @@
 
 #else
 #error "OS not supported!"
-#endif // _WIN32
+#endif
 
 typedef struct {
-    int length;
+    uint32_t size;
     char* start;
 } _file_line;
 
 typedef struct {
-    int line_count;
+    uint32_t line_count;
     _file_line* lines;
 } _file;
 
-int file_read_get_lines(FILE* fptr) {
+/*
+ *  This function returns the number of lines in a file,
+ *  as detected with the OS specific new line character.
+ *  
+ *  Input: FILE* fptr of file.
+ *  Output: 
+ *      - If fptr is NULL, returns -1
+ *      - If fptr is NOT NULL, returns the number of lines.
+ */
+uint32_t file_read_get_lines(FILE* fptr) {
     if (fptr == NULL) return -1;
     rewind(fptr);
 
     char buffer[BUFFER_SIZE];
-    int lines = 0;
+    uint32_t lines = 0;
 
     while(1) {
         size_t bytes = fread(buffer, 1, BUFFER_SIZE, fptr);
@@ -58,11 +69,31 @@ int file_read_get_lines(FILE* fptr) {
 
 _file* file_read(FILE* fptr) {
     if (fptr == NULL) return NULL;
-    int line_count = file_read_get_lines(fptr);
-    _file_line* lines;
 
+    uint32_t line_count = file_read_get_lines(fptr);
+    _file* file = (_file*)calloc(1, sizeof(_file*));
+    file->line_count = file_read_get_lines(fptr);
+    // file->lines = (_file_line*)calloc(line_count, sizeof(_file_line*));
+    file->lines = NULL;
+    //
+    // char* line = NULL;
+    // size_t size = 0;
+    // ssize_t line_size;
+    // _file_line* fline;
+    // int count = 0;
+    //
+    // while(line_size = getline(&line, &size, fptr) != -1) {
+    //     fline = calloc(1, sizeof(uint32_t) + (line_size * sizeof(char)));
+    //     fline->size = (uint32_t)line_size;
+    //     fline->line = *line;
+    //
+    //     // char* temp_line = calloc(fline->size, sizeof(char));
+    //     fline->start = line;
+    //     file->lines[count] = fline;
+    //
+    //     count++;
+    //     fline = NULL;
+    // }
 
-    _file file = {
-        .line_count = file_read_get_lines(fptr),
-    };
+    return file;
 }
