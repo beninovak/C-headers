c951bbeb56f3f08c5a8fac5677c205ebc1a58401
beninovak <beninonovakino@proton.me>
2025-05-18 11:56:15 +0200

    Headers

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include "read_file.h"
+
+int main() {
+    clock_t start_time = clock();
+
+    FILE* fptr = fopen("text_1gb.txt", "rb");
+    long num_lines = file_read_get_lines(fptr);
+
+    printf("Number of lines: %ld", num_lines);
+
+    double total_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+    printf("\nTime elapsed: %f\n", total_time);
+    return 0;
+}
--- read_file.h
+++ read_file.h
@@ -1,5 +1,22 @@
 #pragma once
-#include <cstdio>
+#include <stdio.h>
+
+//#define BUFFER_SIZE 16777216 -> TOO BIG
+#define BUFFER_SIZE 65536
+
+
+#ifdef _WIN32
+#define NEWLINE_CHAR '\r'
+
+#elif __linux__
+#define NEWLINE_CHAR '\n'
+
+#elif __APPLE__
+#define NEWLINE_CHAR '\n'
+
+#else
+#error "OS not supported!"
+#endif // _WIN32
 
 typedef struct {
     int length;
@@ -13,19 +30,28 @@ typedef struct {
 
 int file_read_get_lines(FILE* fptr) {
     if (fptr == NULL) return -1;
-
     rewind(fptr);
 
-    char ch;
+    char buffer[BUFFER_SIZE];
     int lines = 0;
 
-    while(!feof(fptr)) {
-        ch = fgetc(fptr);
+    while(1) {
+        size_t bytes = fread(buffer, 1, BUFFER_SIZE, fptr);
+        if (ferror(fptr)) {
+            return -1;
+        }
 
-        if (ch == '\n') {
-            lines++;
+        for(int i = 0; i < bytes; i++) {
+            if (buffer[i] == NEWLINE_CHAR) {
+                lines++;
+            }
+        }
+
+        if (feof(fptr)) {
+            break;
         }
     }
+    
     rewind(fptr);
 
     return lines;
